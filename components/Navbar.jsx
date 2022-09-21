d2f0b427d19a5eac6ec9f26a1223bfefd81d7f73
otiisaac199 <otichinazaisaac19@gmail.com>
2022-09-21 10:44:03 +0100

    making menu responsive

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,42 +5,42 @@ import { AiOutlineMenu, AiOutlineCLose } from "react-icons/ai";
 const Navbar = () => {
   return (
     <div className="fixed left-0 top-0 w-full z-10 ease-in duration-300">
-      <div className="max-w-[1240px] m-auto flex justify-between">
+      <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-white">
         <Link href="/">
-          <h1>Captur</h1>
+          <h1 className="font-bold text-4xl">Captur</h1>
         </Link>
-        <ul>
-          <li>
+        <ul className="hidden sm:flex">
+          <li className="p-4">
             <Link href="/">Home</Link>
           </li>
-          <li>
+          <li className="p-4">
             <Link href="/#gallery">Gallery</Link>
           </li>
-          <li>
+          <li className="p-4">
             <Link href="/portfolio">Work</Link>
           </li>
-          <li>
+          <li className="p-4">
             <Link href="/contact">Contact</Link>
           </li>
         </ul>
 
         {/* Mobile Button */}
-        <div>
+        <div className="block md:hidden ">
           <AiOutlineMenu size={20} />
         </div>
         {/* Mobile Menu */}
-        <div>
+        <div className="sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300 ">
           <ul>
-            <li>
+            <li className="p-4 text-4xl hover:text-gray-500">
               <Link href="/">Home</Link>
             </li>
-            <li>
+            <li className="p-4 text-4xl hover:text-gray-500">
               <Link href="/#gallery">Gallery</Link>
             </li>
-            <li>
+            <li className="p-4 text-4xl hover:text-gray-500">
               <Link href="/portfolio">Work</Link>
             </li>
-            <li>
+            <li className="p-4 text-4xl hover:text-gray-500">
               <Link href="/contact">Contact</Link>
             </li>
           </ul>
