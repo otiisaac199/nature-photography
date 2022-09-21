2b412f9f4c9ec10c524ef196d4bf580b39391be1
otiisaac199 <otichinazaisaac19@gmail.com>
2022-09-21 14:12:52 +0100

    Features: added images

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,15 +1,42 @@
 import Link from "next/link";
-import React from "react";
-import { AiOutlineMenu, AiOutlineCLose } from "react-icons/ai";
+import React, { useEffect, useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
+  let [nav, setNav] = useState(false);
+
+  const [color, setColor] = useState("transparent");
+  const [textColor, setTextColor] = useState("white");
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  useEffect(() => {
+    const changeColor = () => {
+      if (window.scrollY >= 90) {
+        setColor("#ffff");
+        setTextColor("#000");
+      } else {
+        setColor("transparent");
+        setTextColor("white");
+      }
+    };
+    window.addEventListener("scroll", changeColor);
+  }, []);
+
   return (
-    <div className="fixed left-0 top-0 w-full z-10 ease-in duration-300">
+    <div
+      style={{ backgroundColor: `${color}` }}
+      className="fixed left-0 top-0 w-full z-10 ease-in duration-300"
+    >
       <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-white">
         <Link href="/">
-          <h1 className="font-bold text-4xl">Captur</h1>
+          <h1 style={{ color: `${textColor}` }} className="font-bold text-4xl">
+            Captur
+          </h1>
         </Link>
-        <ul className="hidden sm:flex">
+        <ul style={{ color: `${textColor}` }} className="hidden sm:flex">
           <li className="p-4">
             <Link href="/">Home</Link>
           </li>
@@ -25,11 +52,22 @@ const Navbar = () => {
         </ul>
 
         {/* Mobile Button */}
-        <div className="block md:hidden ">
-          <AiOutlineMenu size={20} />
+        <div onClick={handleNav} className="block sm:hidden z-10 ">
+          {nav ? (
+            <AiOutlineClose size={20} style={{ color: `${textColor}` }} />
+          ) : (
+            <AiOutlineMenu size={20} style={{ color: `${textColor}` }} />
+          )}
         </div>
         {/* Mobile Menu */}
-        <div className="sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300 ">
+        <div
+          style={{ color: `${textColor}` }}
+          className={
+            nav
+              ? "sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300 "
+              : "sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300 "
+          }
+        >
           <ul>
             <li className="p-4 text-4xl hover:text-gray-500">
               <Link href="/">Home</Link>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Hero from "../components/Hero";
+import Slider from "../components/Slider";
+import { SliderData } from "../components/SliderData";
 
 export default function Home() {
   return (
@@ -13,6 +15,7 @@ export default function Home() {
         heading="Captur Photography"
         message="I capture moments in nature and keep them alive."
       />
+      <Slider slides={SliderData} />
     </div>
   );
 }
