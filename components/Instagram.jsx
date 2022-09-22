97591ff5556646784361eb1a3c1f1aca8d88df1c
otiisaac199 <otichinazaisaac19@gmail.com>
2022-09-22 10:12:19 +0100

    reduced the umber of ig images

--- components/Instagram.jsx
+++ components/Instagram.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import IgImg1 from "../public/images/ig-image-1.jpg";
-import IgImg2 from "../public/images/ig-image-2.jpg";
-import IgImg3 from "../public/images/ig-image-3.jpg";
-import IgImg4 from "../public/images/ig-image-4.jpg";
 import IgImg5 from "../public/images/ig-image-5.jpg";
 import IgImg6 from "../public/images/ig-image-6.jpg";
 import IgImg7 from "../public/images/ig-image-7.jpg";
@@ -15,9 +12,6 @@ const Instagram = () => {
       <p className="pb-4">@Webbby_345</p>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 p-4">
         <InstagramImg socialImg={IgImg1} />
-        <InstagramImg socialImg={IgImg2} />
-        <InstagramImg socialImg={IgImg3} />
-        <InstagramImg socialImg={IgImg4} />
         <InstagramImg socialImg={IgImg5} />
         <InstagramImg socialImg={IgImg6} />
         <InstagramImg socialImg={IgImg7} />
